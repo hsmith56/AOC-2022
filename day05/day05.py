60253560db94d15125e80fe6c10e583098e299c6
hsmith56 <47962432+hsmith56@users.noreply.github.com>
2022-12-05 09:29:04 -0500

    absolutely terrible

--- day05/day05.py
+++ day05/day05.py
@@ -3,14 +3,92 @@ import random, math
 def load():
 	with open('day05/day05Input.txt') as file:
 		data = file.readlines()
-		d = [line.strip() for line in data]
+		d = [line.rstrip() for line in data]
 	return d
 
 inpt = load()
 def part1(inpt):
-	return inpt
+	board = []
+	mv_list = []
+	columns = {}
+	moves = 0
+	for x, y in enumerate(inpt):
+		if y.replace(" ", "").isdecimal():
+			moves = x + 2
+			break
+		row = []
+		for i in range(1, len(y), 4):
+			if y[i].isalpha():
+				row.append(y[i])
+			else:
+				row.append('_')
+		board.append(row)
+	board_len = max([len(x) for x in board])
+	for row in board:
+		if len(row) != board_len:
+			row += (['_'] * (board_len - len(row)))
+
+	for x, y in enumerate(board):
+		for i in range(len(y)):
+			try:
+				columns[i].append(board[x][i])
+			except:
+				columns[i] = [board[x][i]]
+	for column in columns:
+		columns[column] = [x for x in columns[column] if x != '_']
+
+	for x in inpt[moves::]:
+		number, start, end = int(x.split(" ")[1]), int(x.split(" ")[3])-1, int(x.split(" ")[5])-1
+		while number != 0:
+			top = columns[start].pop(0)
+			columns[end].insert(0, top)
+			number -= 1
+
+	ans = ""
+	for x in columns:
+		ans += columns[x][0]
+	return ans
 
 def part2(inpt):
-	return 0
+	board = []
+	mv_list = []
+	columns = {}
+	moves = 0
+	for x, y in enumerate(inpt):
+		if y.replace(" ", "").isdecimal():
+			moves = x + 2
+			break
+		row = []
+		for i in range(1, len(y), 4):
+			if y[i].isalpha():
+				row.append(y[i])
+			else:
+				row.append('_')
+		board.append(row)
+	board_len = max([len(x) for x in board])
+	for row in board:
+		if len(row) != board_len:
+			row += (['_'] * (board_len - len(row)))
+
+	for x, y in enumerate(board):
+		for i in range(len(y)):
+			try:
+				columns[i].append(board[x][i])
+			except:
+				columns[i] = [board[x][i]]
+	for column in columns:
+		columns[column] = [x for x in columns[column] if x != '_']
+
+	for x in inpt[moves::]:
+		number, start, end = int(x.split(" ")[1]), int(x.split(" ")[3])-1, int(x.split(" ")[5])-1
+		top = columns[start][0:number]
+		del columns[start][0:number]
+		columns[end] = top + columns[end]
+		number -= 1
+
+	ans = ""
+	for x in columns:
+		ans += columns[x][0]
+	return ans
 
 print(f'Part1: {part1(inpt)}\nPart2: {part2(inpt)}')
