059c3eca87eaf7cb636bab538994dc167758dfc3
hsmith56 <47962432+hsmith56@users.noreply.github.com>
2022-12-06 08:38:04 -0500

    day 2 done in rust

--- day02-rust/src/main.rs
+++ day02-rust/src/main.rs
@@ -7,11 +7,11 @@ fn part_1(input: &String) {
         .filter(|line| !line.is_empty())
         .map(|line| ((line.as_bytes()[0] - b'A') as u16, (line.as_bytes()[2] - b'X') as u16))
         .map(|(a, b)| match (a, b) {
-            (a, b ) if a == 0 && b == 2 => 1 + b,
-            (a, b ) if a == 2 && b == 0 => 7 + b,
-            (a, b ) if a > b => 1 + b,
-            (a, b ) if a < b => 7 + b,
-            (a, b ) if a == b => 4 + b, 
+            (0, 2) => 1 + b,
+            (2, 0) => 7 + b,
+            (a, b) if a > b => 1 + b,
+            (a, b) if a < b => 7 + b,
+            (a, b) if a == b => 4 + b, 
             (_, _) => 0
         })
         .sum::<u16>();
@@ -21,12 +21,23 @@ fn part_1(input: &String) {
 }
 
 fn part_2(input: &String) {
-    let mut score = 0;
-    println!("Day 1 Part 2: {}", score);
+    let idk = input
+    .lines()
+    .filter(|line| !line.is_empty())
+    .map(|line| ((line.as_bytes()[0] - b'A') as i32, (line.as_bytes()[2] - b'X') as i32))
+    .map(|(a, b)| match (a, b) {
+        (0, 0) => 3,
+        (2, 2) => 7,
+        (a, 0) => a,
+        (a, 1) => 4 + a,
+        (a, 2) => 8 + a,
+        (_, _) => 0
+    }).sum::<i32>();
+    println!("Day 1 Part 2: {:?}", idk);
 }
 
 fn main() {
-    let file_path = "input.txt";
+    let file_path = "example.txt";
     println!("In file {}", file_path);
 
     let input = fs::read_to_string(file_path)
